2fe32be336f1300bda50aa976f3995f7dc9ef068
Guilherme Berson <bersondev@gmail.com>
2020-05-26 09:52:16 -0300

    dasfsdfsdfs

--- sorteialetra/sorteador.js
+++ sorteialetra/sorteador.js
@@ -1,6 +1,6 @@
 var letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-document.getElementById("letra").innerHTML =  "-"
+document.getElementById("letra").innerHTML =  "Start"
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
