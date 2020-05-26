7979956588a691009be17b85135f026692921306
Guilherme Berson <bersondev@gmail.com>
2020-05-26 09:44:18 -0300

    sorteador de letrss

---
+++ SorteaLetra/sorteador.js
@@ -0,0 +1,13 @@
+var letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+document.getElementById("letra").innerHTML =  "-"
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sorteiaLetra() {
+    var randonNumber = getRandomInt(0,25)
+    var letraSorteada = letras[randonNumber]
+    document.getElementById("letra").innerHTML = letraSorteada.toUpperCase()
+}
